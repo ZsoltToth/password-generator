0a58777166cc1411c1d70300dbfcd9519f46d741
Zsolt Toth <tothzs87@gmail.com>
2025-12-16 06:50:58 +0100

    fix: use expo-crypto instead of password-generator lib

--- store/passwordsSlice.ts
+++ store/passwordsSlice.ts
@@ -1,5 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
-import passwordGenerator from "password-generator";
+import * as Crypto from 'expo-crypto'
 
 export const passwordSlice = createSlice({
     name: 'passwordSlice',
@@ -18,9 +18,17 @@ export const passwordSlice = createSlice({
 export const generatePasswords = (length: number, count: number) => {
     let passwords: string[] = [];
     for (let i = 0; i < count; i++) {
-        passwords.push(passwordGenerator(length));
+        passwords.push(generatePassword(length));
     }
     return passwordSlice.actions.setPasswords(passwords);
+}
+
+export const generatePassword = (length: number) : string => {
+    let randomBytes : Uint8Array = Crypto.getRandomBytes(length);
+    const mappedBytes = randomBytes.map(b => (b % 25) + 65);
+    const textDecoder = new TextDecoder();
+    const result = textDecoder.decode(mappedBytes);
+    return result;
 }
 
 export const selectPasswords = (state: any): string[] => state.passwords.passwords;
